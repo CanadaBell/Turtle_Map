6e23afda3b8170f78fe16f3666a5394eb9e8f3c1
CanadaBell <123008017+CanadaBell@users.noreply.github.com>
2023-04-17 14:00:01 -0600

    Create kill me now.py

---
+++ kill me now.py
@@ -0,0 +1,22 @@
+import turtle
+# import random
+# end_x = random.randrange(-400,400,5)
+# end_y = random.randrange(-400,400,5)
+# file = open("cords.txt", "w")
+# file.write(f"up\n{st_x}\t{st_y}\ndown")
+# file.close()
+
+w = turtle.Screen()
+# uber = turtle.Turtle()
+s = turtle.Turtle()
+# w.setup(800, 800)
+# w.screensize(400, 400)
+# for x in range(4):
+#     s.fd(400)
+#     s.bk(400)
+#     s.lt(90)
+inst = open("cords.txt", "r")
+inst = inst.readlines()
+inst = ("".join(inst)).split()
+print (inst)
+s.'up'
