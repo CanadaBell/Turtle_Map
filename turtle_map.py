160c525a178657546bac36671ce04e3214c8297c
CanadaBell <123008017+CanadaBell@users.noreply.github.com>
2023-04-18 13:28:43 -0600

    Changed how the code looked
    Added comments
    Put an if statement together with a semi colon
    Put the cord vars on same line with a comma

--- turtle_map.py
+++ turtle_map.py
@@ -2,10 +2,8 @@
 import turtle
 import random
 ## File ##
-st_x = random.randrange(-300,300,5) # get random cords for start pos
-st_y = random.randrange(-300,300,5)
-end_x = random.randrange(-300,300,5) # get random cords for end pos
-end_y = random.randrange(-300,300,5)
+st_x, st_y = random.randrange(-300,300,5), random.randrange(-300,300,5) # get random cords for start pos
+end_x, end_y = random.randrange(-300,300,5), random.randrange(-300,300,5) # get random cords for end pos
 file = open("cords.txt", "w") 
 file.write(f"up\n{st_x}\t{st_y}\n{end_x}\t{end_y}\ndown") # writes to a txt document: up, start pos, end pos, down
 file.close()
@@ -14,8 +12,7 @@ w = turtle.Screen() # screen
 uber = turtle.Turtle() # turtle
 s = turtle.Turtle() # turtle
 # Window Settings #
-w.setup(800, 800)
-w.screensize(400, 400)
+w.setup(800, 800); w.screensize(400, 400)
 # "Uber" Settings #
 uber.hideturtle()
 uber.shape("circle")
@@ -35,24 +32,24 @@ exec("s." + inst[0] + "()") # takes the up from the text file and runs it a turt
 s.goto(int(inst[1]), int(inst[2])) # takes the first 2 numbers from the text file and makes the turtle go to that pos
 exec("s." + inst[5] + "()")# takes the down from the text file and runs it a turtle command
 s.dot(30, "#3B9EBF")# Draws a dot
-x_path = int(inst[3]) - int(inst[1]) #Takes the distance from the second set of cords - the first set of cords (x)
-y_path = int(inst[4]) - int(inst[2]) #Takes the distance from the second set of cords - the first set of cords (y)
+x_path, y_path = int(inst[3]) - int(inst[1]), int(inst[4]) - int(inst[2]) #Takes the distance from the second set of cords - the first set of cords
 s.fd(x_path) # Moves the turtle by distace (x)
 s.lt(90)
 s.fd(y_path) # Moves the turtle by distace (x)
 s.dot(30, "#3B9EBF") # Draws second dot
-eval("uber." + inst[0] + "()") #Does the same thing as the first turtle: (goes to pos) this time the turtle color is white / blue
-uber.goto(int(inst[1]), int(inst[2]))
+eval("uber." + inst[0] + "()") 
+uber.goto(int(inst[1]), int(inst[2])) #Goes to starting pos via same instructions as other turtle
 eval("uber." + inst[5] + "()")
 uber.showturtle()
-uber.dot(15, "white")
-uber.fd(x_path)
-uber.lt(90)
-uber.fd(y_path)
-uber.dot(15, "white")
-phone = int(turtle.numinput("Phone Number", "Please enter your phone number", default=None, minval=0, maxval=9999999999)) 
+uber.dot(15, "white") #Draws smaller dot
+uber.fd(x_path) # Follows same path
+uber.lt(90) # V
+uber.fd(y_path) # V
+uber.dot(15, "white") #Draws smaller dot
+phone = turtle.textinput("Phone Number", "Please enter your phone number") #ask user to input a phone number
+if phone == None: phone = "0"; phone = int(phone) if phone.isdigit() else "0" # Checks if the phone number is a number, if yes keep the same otherwise turns it to a zero 
 w.exitonclick()
 ## File Again ##
 recipt = open("recipt.txt", "w") # Opens file 
 recipt.write(f"up\n{st_x}\t{st_y}\n{end_x}\t{end_y}\ndown\n{phone}") # Writes data from cords + phone number (inputs after drawing)
-recipt.close() # Closes file
+recipt.close() # Closes file 
